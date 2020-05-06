75ade9af403264246c959af6e1986dd70a0716e3
Connor Peet <connor@peet.io>
2020-05-06 15:55:58 -0700

    fix: stub fs.access for tests on other platforms

--- src/darwinFinderBase.test.ts
+++ src/darwinFinderBase.test.ts
@@ -7,8 +7,25 @@ import { resolve } from 'path';
 import execa from 'execa';
 import { DarwinEdgeBrowserFinder } from './darwinEdge';
 import { expect } from 'chai';
+import { stub, SinonStub } from 'sinon';
 
 describe('DarwinFinderBase', () => {
+  const validPaths = new Set([
+    '/Applications/Microsoft Edge Dev.app/Contents/MacOS/Microsoft Edge Dev',
+    '/Applications/Microsoft Edge Beta.app/Contents/MacOS/Microsoft Edge Beta',
+  ]);
+
+  let accessStub: SinonStub;
+  beforeEach(() => {
+    accessStub = stub(fs, 'access').callsFake(path =>
+      validPaths.has(path.toString()) ? Promise.resolve() : Promise.reject('no acccess'),
+    );
+  });
+
+  afterEach(() => {
+    accessStub.restore();
+  });
+
   class ReplacingRegisterFinder extends DarwinEdgeBrowserFinder {
     constructor(file: string) {
       super({}, fs, execa);
